deaac0e4443b7b3dda385934e937368341266194
Shaontae Robinson <shaontae3@gmail.com>
2023-08-24 21:22:44 -0400

    trial-1

---
+++ Develop/script.js
@@ -0,0 +1,55 @@
+// Wrap all code that interacts with the DOM in a call to jQuery to ensure that
+// th ecode isn't run until the browser has finished rendering all the elements
+// in the html.
+$(document).ready(function(){
+}
+  
+  // TODO: Add a listener for click events on the save button. This code should
+  // use the id in the containing time-block as a key to save the user input in
+  // local storage. HINT: What does `this` reference in the click listener
+  // function? How can DOM traversal be used to get the "hour-x" id of the
+  // time-block containing the button that was clicked? How might the id be
+  // useful when saving the description in local storage?
+
+  // TODO: Add code to apply the past, present, or future class to each time
+  // block by comparing the id to the current hour. HINTS: How can the id
+  // attribute of each time-block be used to conditionally add or remove the
+  // past, present, and future classes? How can Day.js be used to get the
+  // current hour in 24-hour time?
+ 
+  let input = localStorage.getItem('input')
+
+
+  // TODO: Add code to get any user input that was saved in localStorage and set
+  // the values of the corresponding textarea elements. HINT: How can the id
+  // attribute of each time-block be used to do this?
+  //
+  // TODO: Add code to display the current date in the header of the page.
+  
+  $(document).ready(function(){
+    $('#currentDay').text(moment().format("MMM Do YYYY h:mm a"));
+
+function time () {
+  $(".time-block").each(function() {    
+    const liveHour = moment().hours();
+    const timeBHour = parseInt($(this).attr('id'));    
+        if (timeBHour < liveHour) {
+            $(this).addClass('past');
+            $(this).removeClass('present future');
+
+        }else if (timeBHour === liveHour) {
+            $(this).addClass('present');
+            $(this).removeClass('past future');
+
+        }else {
+            $(this).addClass('future');
+            $(this).removeClass('past present');
+        }
+    }) 
+};
+
+$("#btn saveBtn").click(function() {
+  alert($("#save").text());
+  $("#p1").text("New Text.");
+});
+  });
